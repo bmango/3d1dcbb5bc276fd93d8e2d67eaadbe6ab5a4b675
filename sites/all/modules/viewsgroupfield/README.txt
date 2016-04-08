ddaa6c506457b0294cdca25e8c54e0fd81d0a363
Ben Mango <ben@benmango.co.uk>
2016-04-08 17:52:27 +0100

    new modules

---
+++ sites/all/modules/viewsgroupfield/README.txt
@@ -0,0 +1,3 @@
+Views Group Fields allows you to group fields in a view row. 
+
+It does this by allowing you to put various fields into groups in a view row. So you can put the body and title in a wrapping div with a class you specify. 
---
+++ sites/all/modules/viewsgroupfield/theme/viewsgroupfield-view-fields.tpl.php
@@ -0,0 +1,21 @@
+<?php foreach ($grouped as $group => $fields): ?>
+
+  <div class='group-<?php print $group ?>'>
+
+    <?php foreach ($fields as $id => $field): ?>
+      <?php if (!empty($field->separator)): ?>
+        <?php print $field->separator; ?>
+      <?php endif; ?>
+
+      <?php print $field->wrapper_prefix; ?>
+        <?php print $field->label_html; ?>
+        <?php print $field->content; ?>
+      <?php print $field->wrapper_suffix; ?>
+    <?php endforeach; ?>
+
+  </div>
+
+<?php endforeach; ?>
+
+
+
